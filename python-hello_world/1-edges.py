38740f94a4ab44235b112328fd8779685222e863
auroretoni <auroretoni@gmail.com>
2023-07-20 00:58:54 +0300

    copy cut and paste

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+word = "Holberton"
+size = len(word)
+slice = word
+print("First 3 letters: {}".format(word[:3]))
+print("Last 2 letters: {}".format(word[-2:]))
+print("Middle word: {}".format(word[1:-1]))
