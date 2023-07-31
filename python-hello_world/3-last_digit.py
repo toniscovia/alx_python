4357ee25f8bf875f0afb53660d410d1560f8ab6e
auroretoni <auroretoni@gmail.com>
2023-08-01 00:29:06 +0300

    commitupdated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -5,11 +5,14 @@ last_digit = abs(number) % 10
 
 if number < 0:
      last_digit = - last_digit
-     
+
 if last_digit > 5:
     comparison = 'greater than 5'
+    print('Last digit of {} is {} and is {}'.format(number, last_digit, comparison))
+
 elif last_digit == 0:
       comparison = '0'
+      print('Last digit of {} is {} and is {}'.format(number, last_digit, comparison))
 else:
      comparison = 'less than 6 and not 0'
      print('Last digit of {} is {} and is {}'.format(number, last_digit, comparison))
