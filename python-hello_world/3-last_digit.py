945409933da1a7a370c1de3e6f374da80e13b297
auroretoni <auroretoni@gmail.com>
2023-08-01 00:22:26 +0300

    commitupdated

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,6 +3,9 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
+if number < 0:
+     last_digit = - last_digit
+     
 if last_digit > 5:
     comparison = 'greater than 5'
 elif last_digit == 0:
