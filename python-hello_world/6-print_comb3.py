4c56fbe432b6a1e52b8a6d34f08c322bd508ccbd
auroretoni <auroretoni@gmail.com>
2023-08-01 20:09:00 +0300

    commitupdated

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,3 @@
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=', ' if i < 8 or j < 9 else '\n')
+        print("{i}{j}".format(i=i, j=j), end=', '  if i < 8 or j < 9 else '\n')
